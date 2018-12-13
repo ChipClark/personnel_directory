2f93f8d00c540bc12ebc498880d51fdaf52298f4
Rodolfo Zuluaga <RodolfoJZuluaga@gmail.com>
2018-12-13 09:07:21 -0800

    added client side pagination

--- src/app/people/people.component.ts
+++ src/app/people/people.component.ts
@@ -32,7 +32,7 @@ import { connectableObservableDescriptor } from 'rxjs/internal/observable/Connec
 })
 
 //export class SendService {
-  // see: https://stackoverflow.com/questions/44205950/angular-passing-a-variable-from-a-component-to-a-service 
+// see: https://stackoverflow.com/questions/44205950/angular-passing-a-variable-from-a-component-to-a-service 
 //  private skip = 20;
 //  private limit = 20;
 //  private lastRecord;
@@ -47,7 +47,7 @@ import { connectableObservableDescriptor } from 'rxjs/internal/observable/Connec
 //    return this.lastRecord;
 //  }
 //}
- 
+
 export class PeopleComponent implements OnInit {
 
   private baseURL = 'http://am-web05:3030/api/people/';
@@ -74,11 +74,12 @@ export class PeopleComponent implements OnInit {
 
   public cityLabelID = "city6";
   public roleLabelID = "Role1";
-  
-  public skip = 0;
+
+  public pageNumber = 0;
   public limit = 20;
   public lastRecord;
-  
+  public lastPage;
+
   private pagination = '"limit":' + this.limit + ',"skip":' + this.skip + ',';
   private order = '"order":"lastname ASC",'
   private personURL = this.baseURL + this.addFilter + this.pagination + this.order + this.generalIncludes + this.endRequest;  // URL to web api
@@ -99,12 +100,13 @@ export class PeopleComponent implements OnInit {
   roles: HRDepartments[];
   legalDepts: LegalDepartments[];
   legalSubDepts: LegalSubDepartments[];
-  
+  activePeople: Person[];
+
   constructor(
     private staffService: APIService,
     private http: HttpClient,
     protected sanitizer: DomSanitizer
-    ) { }
+  ) { }
 
   ngOnInit() {
     this.getPeople();
@@ -113,9 +115,9 @@ export class PeopleComponent implements OnInit {
   onSelect(personid: number): void {
     var primary = this.people.find(obj => {
       return obj.pkpersonid === personid;
-      });
-      
-      console.log(primary.firstname);
+    });
+
+    console.log(primary.firstname);
     //this.router.  ('/details/' + people.pkpersonid);
     //this.selectedPerson.pkpersonid = people.pkpersonid;
   }
@@ -123,37 +125,47 @@ export class PeopleComponent implements OnInit {
   getPeople(): void {
     this.buildURL();
     let JUNK$ = this.staffService.getDATA(this.personURL)
-        .subscribe(people => this.people = people
-        );
+      .subscribe(people => { 
+        this.people = people;
+        this.lastRecord = people.length;
+        this.lastPage = Math.ceil(this.lastRecord / this.limit);
+        this.paginate();
+       }
+      );
   }
-  
-  buildURL () {
+
+  paginate(): void {
+    --this.limit;
+    this.activePeople = this.people.slice(this.limit * this.pageNumber, (this.pageNumber + 1) * this.limit);
+  }
+
+  buildURL() {
     this.pagination = '"limit":' + this.limit + ',"skip":' + this.skip + ',';
     this.buildAddFilter();
     this.personURL = this.baseURL + this.addFilter + this.pagination + this.order + this.generalIncludes + this.endRequest;  // URL to web api
   }
 
-  buildAddFilter () {
+  buildAddFilter() {
     this.addFilter = this.peopleFilter + this.location + this.hrdepartmentFilter + this.EndFilter;
   }
-  
+
 
   //getMorePeople(direction: string, lastRecord): void {
   getMorePeople(direction: string, recordcount: number): void {
-    if(direction == "prev"){
-      if(this.skip >= 20) {
-          this.skip = this.skip - this.limit;
+    if (direction == "prev") {
+      if (this.skip >= 20) {
+        this.skip = this.skip - this.limit;
       }
     }
     else {
       var tempCount = recordcount - (this.limit + this.skip);  // this ensures we don't exceed the totalcount (recordcount)
-      if(this.skip >= 0 && this.skip < recordcount) {
-        if(tempCount > 0) {
+      if (this.skip >= 0 && this.skip < recordcount) {
+        if (tempCount > 0) {
           this.skip = this.skip + this.limit;
         }
       }
     }
-    this.getPeople(); 
+    this.getPeople();
   }
 
   getPeopleByLocation(id: string): void {
@@ -162,49 +174,49 @@ export class PeopleComponent implements OnInit {
     LabelElement = document.getElementById(this.cityLabelID);
     LabelElement.className = "btn btn-outline-secondary";
 
-    switch(id) {
+    switch (id) {
       case "city6":
-          this.location = '';
-          LabelElement = document.getElementById("city6");
-          LabelElement.className = "btn btn-outline-secondary focus active";
-          this.cityLabelID = "city6";
-         break;
-      case "city1": 
-          this.location = ',"officelocationid":4';
-          LabelElement = document.getElementById("city1");
-          LabelElement.className = "btn btn-outline-secondary focus active";
-          this.cityLabelID = "city1";
-         break;
+        this.location = '';
+        LabelElement = document.getElementById("city6");
+        LabelElement.className = "btn btn-outline-secondary focus active";
+        this.cityLabelID = "city6";
+        break;
+      case "city1":
+        this.location = ',"officelocationid":4';
+        LabelElement = document.getElementById("city1");
+        LabelElement.className = "btn btn-outline-secondary focus active";
+        this.cityLabelID = "city1";
+        break;
       case "city2":
-          this.location = ',"officelocationid":1';
-          LabelElement = document.getElementById("city2");
-          LabelElement.className = "btn btn-outline-secondary focus active";
-           this.cityLabelID = "city2";
-         break;
+        this.location = ',"officelocationid":1';
+        LabelElement = document.getElementById("city2");
+        LabelElement.className = "btn btn-outline-secondary focus active";
+        this.cityLabelID = "city2";
+        break;
       case "city3":
-          this.location = ',"officelocationid":2';
-          LabelElement = document.getElementById("city3");
-          LabelElement.className = "btn btn-outline-secondary focus active";
-           this.cityLabelID = "city3";
-         break;
+        this.location = ',"officelocationid":2';
+        LabelElement = document.getElementById("city3");
+        LabelElement.className = "btn btn-outline-secondary focus active";
+        this.cityLabelID = "city3";
+        break;
       case "city4":
-          this.location = ',"officelocationid":3';
-          LabelElement = document.getElementById("city4");
-          LabelElement.className = "btn btn-outline-secondary focus active";
-          this.cityLabelID = "city4";
-         break;
+        this.location = ',"officelocationid":3';
+        LabelElement = document.getElementById("city4");
+        LabelElement.className = "btn btn-outline-secondary focus active";
+        this.cityLabelID = "city4";
+        break;
       case "city5":
-          this.location = ',"officelocationid":5';
-          LabelElement = document.getElementById("city5");
-          LabelElement.className = "btn btn-outline-secondary focus active";
-          this.cityLabelID = "city5";
-         break;
-      default: 
-         this.location = "";
+        this.location = ',"officelocationid":5';
+        LabelElement = document.getElementById("city5");
+        LabelElement.className = "btn btn-outline-secondary focus active";
+        this.cityLabelID = "city5";
+        break;
+      default:
+        this.location = "";
         break;
     }
     this.skip = 0;
-    this.getPeople(); 
+    this.getPeople();
   }
 
   getPeopleByRole(id: string): void {
@@ -212,93 +224,93 @@ export class PeopleComponent implements OnInit {
     LabelElement = document.getElementById(this.roleLabelID);
     LabelElement.className = "small-font btn btn-outline-secondary";
 
-    switch(id) {
+    switch (id) {
       case "Role1":
-          this.hrdepartmentFilter = '';
-          LabelElement = document.getElementById("Role1");
-          LabelElement.className = "small-font btn btn-outline-secondary focus active";
-          this.roleLabelID = "Role1";
-         break;
-      case "Role2": 
-          this.hrdepartmentFilter = ',"hrdepartment":13';
-          LabelElement = document.getElementById("Role2");
-          LabelElement.className = "small-font btn btn-outline-secondary focus active";
-          this.roleLabelID = "Role2";
-         break;
+        this.hrdepartmentFilter = '';
+        LabelElement = document.getElementById("Role1");
+        LabelElement.className = "small-font btn btn-outline-secondary focus active";
+        this.roleLabelID = "Role1";
+        break;
+      case "Role2":
+        this.hrdepartmentFilter = ',"hrdepartment":13';
+        LabelElement = document.getElementById("Role2");
+        LabelElement.className = "small-font btn btn-outline-secondary focus active";
+        this.roleLabelID = "Role2";
+        break;
       case "Role3":
-          this.hrdepartmentFilter = ',"hrdepartment":1';
-          LabelElement = document.getElementById("Role3");
-          LabelElement.className = "small-font btn btn-outline-secondary focus active";
-          this.roleLabelID = "Role3";
-         break;
+        this.hrdepartmentFilter = ',"hrdepartment":1';
+        LabelElement = document.getElementById("Role3");
+        LabelElement.className = "small-font btn btn-outline-secondary focus active";
+        this.roleLabelID = "Role3";
+        break;
       case "Role4":
-          this.hrdepartmentFilter = ',"hrdepartment":11';
-          LabelElement = document.getElementById("Role4");
-          LabelElement.className = "small-font btn btn-outline-secondary focus active";
-          this.roleLabelID = "Role4";
-         break;
+        this.hrdepartmentFilter = ',"hrdepartment":11';
+        LabelElement = document.getElementById("Role4");
+        LabelElement.className = "small-font btn btn-outline-secondary focus active";
+        this.roleLabelID = "Role4";
+        break;
       case "Role5":
-          this.hrdepartmentFilter = ',"hrdepartment":9';
-          LabelElement = document.getElementById("Role5");
-          LabelElement.className = "small-font btn btn-outline-secondary focus active";
-          this.roleLabelID = "Role5";
-         break;
+        this.hrdepartmentFilter = ',"hrdepartment":9';
+        LabelElement = document.getElementById("Role5");
+        LabelElement.className = "small-font btn btn-outline-secondary focus active";
+        this.roleLabelID = "Role5";
+        break;
       case "Role6":
-          this.hrdepartmentFilter = ',"hrdepartment":8';
-          LabelElement = document.getElementById("Role6");
-          LabelElement.className = "small-font btn btn-outline-secondary focus active";
-          this.roleLabelID = "Role6";
-         break;
+        this.hrdepartmentFilter = ',"hrdepartment":8';
+        LabelElement = document.getElementById("Role6");
+        LabelElement.className = "small-font btn btn-outline-secondary focus active";
+        this.roleLabelID = "Role6";
+        break;
       case "Role7":
-         this.hrdepartmentFilter = ',"hrdepartment":7';
-         LabelElement = document.getElementById("Role7");
-         LabelElement.className = "small-font btn btn-outline-secondary focus active";
-         this.roleLabelID = "Role7";
+        this.hrdepartmentFilter = ',"hrdepartment":7';
+        LabelElement = document.getElementById("Role7");
+        LabelElement.className = "small-font btn btn-outline-secondary focus active";
+        this.roleLabelID = "Role7";
         break;
       case "Role8":
-         this.hrdepartmentFilter = ',"hrdepartment":6';
-         LabelElement = document.getElementById("Role8");
-         LabelElement.className = "small-font btn btn-outline-secondary focus active";
-         this.roleLabelID = "Role8";
+        this.hrdepartmentFilter = ',"hrdepartment":6';
+        LabelElement = document.getElementById("Role8");
+        LabelElement.className = "small-font btn btn-outline-secondary focus active";
+        this.roleLabelID = "Role8";
         break;
       case "Role9":
-         this.hrdepartmentFilter = ',"hrdepartment":3';
-         LabelElement = document.getElementById("Role9");
-         LabelElement.className = "small-font btn btn-outline-secondary focus active";
-         this.roleLabelID = "Role9";
+        this.hrdepartmentFilter = ',"hrdepartment":3';
+        LabelElement = document.getElementById("Role9");
+        LabelElement.className = "small-font btn btn-outline-secondary focus active";
+        this.roleLabelID = "Role9";
         break;
       case "Role10":
-         this.hrdepartmentFilter = ',"hrdepartment":12';
-         LabelElement = document.getElementById("Role10");
-         LabelElement.className = "small-font btn btn-outline-secondary focus active";
-         this.roleLabelID = "Role10";
+        this.hrdepartmentFilter = ',"hrdepartment":12';
+        LabelElement = document.getElementById("Role10");
+        LabelElement.className = "small-font btn btn-outline-secondary focus active";
+        this.roleLabelID = "Role10";
         break;
-     default: 
-         this.location = "";
+      default:
+        this.location = "";
         break;
     }
     this.skip = 0;
-    this.getPeople(); 
+    this.getPeople();
   }
 
   getPersonTitles(currentperson: any): string {
     var currentJobTitle = currentperson.jobtitle.jobtitle;
-    
+
     var addHTML = "<strong>" + currentJobTitle + "</strong>";
 
-    if(currentperson.practices.length > 0) {
+    if (currentperson.practices.length > 0) {
       addHTML = addHTML + "<br>";
       var currentPractices = currentperson.practices;
       var i;
-      for(i = 0; i < currentPractices.length; i++){
-        if(i > 0  ) {
+      for (i = 0; i < currentPractices.length; i++) {
+        if (i > 0) {
           addHTML = addHTML + ",";
-          if( i == 2 || i == 4 ) {
+          if (i == 2 || i == 4) {
             addHTML = addHTML + "<br>";
           }
         }
         addHTML = addHTML + " " + currentPractices[i].practicename;
-        
+
       }
       addHTML = addHTML + "<br>";
 
@@ -310,85 +322,85 @@ export class PeopleComponent implements OnInit {
     // this isn't working yet  - photos getting put into database ...
     // no need for extensive logic 
     var personPhoto, noPhoto, photoString;
-    
+
     var personlist = this.people;
 
     var primary = personlist.find(obj => {
       return obj.pkpersonid === 1;
-      });
+    });
 
-      photoString = `<img src="http://amjabber/nophoto.gif" id='{{person.pkperson.id}}' width="112px;" />`
-    
-      var image=new Image();
-      //console.log("in getPhoto()");
-      //personPhoto = "http://amjabber/" + primary.addomainaccount + ".jpg";
-      image.onload = function () {
-        //photoString = '<img src="' + personPhoto + '" id=' + personid + 'width="112px;" />';
-      }
-      image.onerror = function () {
-        //photoString = '<img src="' + personPhoto + '" id=' + personid + 'width="112px;" />';
-      }
-      image.src = personPhoto;
+    photoString = `<img src="http://amjabber/nophoto.gif" id='{{person.pkperson.id}}' width="112px;" />`
 
-      //image.id = personid.toString();
+    var image = new Image();
+    //console.log("in getPhoto()");
+    //personPhoto = "http://amjabber/" + primary.addomainaccount + ".jpg";
+    image.onload = function () {
+      //photoString = '<img src="' + personPhoto + '" id=' + personid + 'width="112px;" />';
+    }
+    image.onerror = function () {
+      //photoString = '<img src="' + personPhoto + '" id=' + personid + 'width="112px;" />';
+    }
+    image.src = personPhoto;
 
-      return photoString;
+    //image.id = personid.toString();
+
+    return photoString;
   }
 
   getEducation(currentperson: any): string {
     var addHTML = "";
     var schoolID;
-    
+
     //currentperson.education.graduationyear
 
     return addHTML;
   }
 
   getEmail(EMAIL: string, personName: string): string {
-    var emailString = '<a href=mailto:' + EMAIL + ' id=' + personName + ' >' + EMAIL + '</a>'; 
-  
-  return emailString;
+    var emailString = '<a href=mailto:' + EMAIL + ' id=' + personName + ' >' + EMAIL + '</a>';
+
+    return emailString;
   }
 
   getPrefName(prefName: string): string {
-    if(!prefName) return "";
-    prefName = '"' + prefName + '"';    
+    if (!prefName) return "";
+    prefName = '"' + prefName + '"';
     return prefName;
   }
 
   getPhone(currentperson: any): SafeHtml | SafeValue {
     var phonetypeid = 1;
-    var officePhone = currentperson.phones.find(obj => { 
+    var officePhone = currentperson.phones.find(obj => {
       return obj.phonetypeid === phonetypeid;
     });
 
-    if(!officePhone) {
+    if (!officePhone) {
       var nophone = 'Phone: <br>'
-      return nophone; 
+      return nophone;
     }
 
     var phonenum = officePhone.phonenumber;
     phonenum = phonenum.replace(/\D+/g, '')
-          .replace(/(\d{3})(\d{3})(\d{4})/, '($1) $2-$3');
+      .replace(/(\d{3})(\d{3})(\d{4})/, '($1) $2-$3');
 
     phonenum = 'Phone: <a href="tel:+' + officePhone.phonenumber + '">' + phonenum + '</a><br>'
     return this.sanitizer.bypassSecurityTrustHtml(phonenum);
   }
 
-  writeAssistant(currentperson: any): SafeHtml  {
-    if(currentperson.personrelationship.length == 0 ) return null;
+  writeAssistant(currentperson: any): SafeHtml {
+    if (currentperson.personrelationship.length == 0) return null;
 
     var asstID = currentperson.personrelationship[0].relatedpersonid;
     var assistants: Person;
     var asst;
 
-    for(let i = 0; i < currentperson.personrelationship.length; i++){
+    for (let i = 0; i < currentperson.personrelationship.length; i++) {
       var detailURL = "/detail/" + currentperson.personrelationship[i].relatedpersonid;
-      asst  = '<a routerLink="' + detailURL
-      + '" href="' + detailURL  + '" >Assistant</a><br>';
-  
-      for(let j = 0; j < this.people.length; j++) {
-        if(currentperson.personrelationship[i].relatedpersonid == this.people[j].pkpersonid) {
+      asst = '<a routerLink="' + detailURL
+        + '" href="' + detailURL + '" >Assistant</a><br>';
+
+      for (let j = 0; j < this.people.length; j++) {
+        if (currentperson.personrelationship[i].relatedpersonid == this.people[j].pkpersonid) {
           assistants = this.people[j];
         }
       }
@@ -396,8 +408,8 @@ export class PeopleComponent implements OnInit {
     return this.sanitizer.bypassSecurityTrustHtml(asst);
   }
 
-  sanitizeScript(sanitizer: DomSanitizer){}
+  sanitizeScript(sanitizer: DomSanitizer) { }
 
-  
-  
+
+
 }
